2a55398904affdff94b7a4e1e95ba4608c92507a
sreevardhanreddi <sreevardhanreddi@gmail.com>
2021-07-23 15:31:51 +0530

    updated; added vim_setup inside full_setup

--- full_setup.sh
+++ full_setup.sh
@@ -4,4 +4,5 @@ rm -rf dotfiles && \
     git clone https://github.com/sreevardhanreddi/dotfiles.git && \ 
     cd dotfiles && \ 
     ./shell_setup.sh && \
-    ./tmux_setup.sh
+    ./tmux_setup.sh && |
+    ./vim_setup.sh
