b51981a9a4eedbfcf2d0157c3cc38c2abd78a279
sreevardhanreddi <sreevardhanreddi@gmail.com>
2022-10-26 16:53:58 +0530

    feat: added autoformatting setup in null-ls

--- nvim/lua/plugins/configs/null-ls.lua
+++ nvim/lua/plugins/configs/null-ls.lua
@@ -1,8 +1,27 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.formatting.eslint,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
-    },
+
+  sources = {
+    require("null-ls").builtins.formatting.stylua,
+    require("null-ls").builtins.formatting.eslint,
+    require("null-ls").builtins.diagnostics.eslint,
+    require("null-ls").builtins.completion.spell,
+  },
+
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+  end,
+
+
+
 })
