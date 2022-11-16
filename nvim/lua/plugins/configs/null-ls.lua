ab94a29a694b3281f3427b2b22111a1d89582a22
sreevardhanreddi <sreevardhanreddi@gmail.com>
2022-11-16 10:29:28 +0530

    feat: added formatting for python

--- nvim/lua/plugins/configs/null-ls.lua
+++ nvim/lua/plugins/configs/null-ls.lua
@@ -4,6 +4,8 @@ require("null-ls").setup({
 
   sources = {
     require("null-ls").builtins.formatting.stylua,
+    require("null-ls").builtins.formatting.isort,
+    require("null-ls").builtins.formatting.black,
     require("null-ls").builtins.formatting.eslint,
     require("null-ls").builtins.formatting.gofmt,
     require("null-ls").builtins.formatting.phpcsfixer,
