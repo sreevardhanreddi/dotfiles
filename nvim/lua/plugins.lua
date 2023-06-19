70fea8f7f94c49337b104237aa71347ae780f7f2
sreevardhanreddi <sreevardhanreddi@gmail.com>
2023-06-19 07:53:09 +0530

    fix: update copilot plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -25,94 +25,96 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 return require("lazy").setup({
-  -- use("wbthomason/packer.nvim") -- Packer can manage itself
-  "nvim-lua/plenary.nvim", -- Common utilities
+	-- use("wbthomason/packer.nvim") -- Packer can manage itself
+	"nvim-lua/plenary.nvim", -- Common utilities
 
-  { "nvim-telescope/telescope.nvim",   tag = "0.1.1" },
+	{ "nvim-telescope/telescope.nvim", tag = "0.1.1" },
 
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      {
-        -- Optional
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
-      { "hrsh7th/cmp-cmdline" },
-      { "saadparwaiz1/cmp_luasnip" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "saadparwaiz1/cmp_luasnip" },
 
-      -- Snippets
-      { "L3MON4D3/LuaSnip" }, -- Snippet engine
-      { "rafamadriz/friendly-snippets" },
-    },
-  },
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Snippet engine
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  "p00f/nvim-ts-rainbow",
-  "akinsho/toggleterm.nvim",
-  { "akinsho/bufferline.nvim",    version = "*" },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"p00f/nvim-ts-rainbow",
+	"akinsho/toggleterm.nvim",
+	{ "akinsho/bufferline.nvim", version = "*" },
 
-  -- auto completion
-  "onsails/lspkind-nvim",
-  "windwp/nvim-autopairs",  -- Autopairs
-  "windwp/nvim-ts-autotag", -- Autotags
+	-- auto completion
+	"onsails/lspkind-nvim",
+	"windwp/nvim-autopairs", -- Autopairs
+	"windwp/nvim-ts-autotag", -- Autotags
 
-  -- colorthemes
-  { "tjdevries/colorbuddy.nvim" },
-  { "projekt0n/github-nvim-theme" },
-  { "folke/tokyonight.nvim" },
-  { "gruvbox-community/gruvbox" },
-  { "martinsione/darkplus.nvim" },
-  {
-    "loctvl842/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup()
-    end,
-  },
+	-- colorthemes
+	{ "tjdevries/colorbuddy.nvim" },
+	{ "projekt0n/github-nvim-theme" },
+	{ "folke/tokyonight.nvim" },
+	{ "gruvbox-community/gruvbox" },
+	{ "martinsione/darkplus.nvim" },
+	{
+		"loctvl842/monokai-pro.nvim",
+		config = function()
+			require("monokai-pro").setup()
+		end,
+	},
 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
 
-  "numToStr/Comment.nvim",           -- Commenting
+	"numToStr/Comment.nvim", -- Commenting
 
-  "nvim-lualine/lualine.nvim",       -- Statusline
+	"nvim-lualine/lualine.nvim", -- Statusline
 
-  "lewis6991/gitsigns.nvim",         -- GitSigns
-  "jose-elias-alvarez/null-ls.nvim", -- Null-ls for autoformatting
+	"lewis6991/gitsigns.nvim", -- GitSigns
+	"jose-elias-alvarez/null-ls.nvim", -- Null-ls for autoformatting
 
-  "folke/which-key.nvim",
+	"folke/which-key.nvim",
 
-  "lukas-reineke/indent-blankline.nvim",
+	"lukas-reineke/indent-blankline.nvim",
 
-  "gpanders/editorconfig.nvim",                    -- editorconfig
+	"gpanders/editorconfig.nvim", -- editorconfig
 
-  { "mg979/vim-visual-multi", branch = "master" }, -- visual multi
+	{ "mg979/vim-visual-multi", branch = "master" }, -- visual multi
 
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+
+	"github/copilot.vim",
 })
--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -1,5 +1,13 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'ff', builtin.find_files, {})
-vim.keymap.set('n', 'fg', builtin.live_grep, {})
-vim.keymap.set('n', 'fb', builtin.buffers, {})
-vim.keymap.set('n', 'fh', builtin.help_tags, {})
+require("telescope").setup({
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-L" },
+		},
+	},
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "ff", builtin.find_files, {})
+vim.keymap.set("n", "fg", builtin.live_grep, {})
+vim.keymap.set("n", "fb", builtin.buffers, {})
+vim.keymap.set("n", "fh", builtin.help_tags, {})
